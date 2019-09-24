f21602d4b83d7f238492b96731ac5f2ecc3a0b14
yzimarova <yana.zimaryova@gmail.com>
2019-09-24 15:43:28 +0300

    Fix Homework1.Exercise2

--- src/main/java/homework1/Exercise2.java
+++ src/main/java/homework1/Exercise2.java
@@ -7,17 +7,11 @@ public class Exercise2 {
 
     public static void cutByDigits() {
         int n = 876543;
-        int n1 = n / 100000;
-        int n2 = (n % 100000) / 10000;
-        int n3 = (n % 10000) / 1000;
-        int n4 = (n % 1000) / 100;
-        int n5 = (n % 100) / 10;
-        int n6 = n % 10;
-        System.out.println(n1);
-        System.out.println(n2);
-        System.out.println(n3);
-        System.out.println(n4);
-        System.out.println(n5);
-        System.out.println(n6);
+        int[] res = new int[6];
+        for (int i = 0; i < 6; i++) {
+            res[i] = n % 10;
+            n = n / 10;
+            System.out.println(res[i]);
+        }
     }
 }
