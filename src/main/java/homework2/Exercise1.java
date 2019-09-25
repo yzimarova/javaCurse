ce6b075821e0180e7e5aeba41a5caa7fbf4af7f4
yzimarova <yana.zimaryova@gmail.com>
2019-09-25 16:29:24 +0300

    Fix Homework2.Exercise1

--- src/main/java/homework2/Exercise1.java
+++ src/main/java/homework2/Exercise1.java
@@ -10,8 +10,8 @@ public class Exercise1 {
         String course = "Курс: Java core";
         String student = "Студент";
         drawStars(getLength(name));
-        drawCourse(name, course);
-        drawStudent(name, student);
+        drawText(name, course);
+        drawText(name, student);
         drawName(name);
         drawStars(getLength(name));
     }
@@ -32,29 +32,13 @@ public class Exercise1 {
         System.out.println("* " + name + " *");
     }
 
-    public static void drawCourse(String name, String course) {
-        int gaps = (name.length() - course.length())/2;
+    public static void drawText(String name, String text) {
+        int gaps = (name.length() - text.length())/2;
         System.out.print("*");
         for (int i = 0; i <= gaps; i++) {
             System.out.print(" ");
         }
-        System.out.print(course);
-        for (int i = 0; i <= gaps; i++) {
-            System.out.print(" ");
-        }
-        if (name.length()%2 == 0) {
-            System.out.print(" ");
-        }
-        System.out.println("*");
-    }
-
-    public static void drawStudent(String name, String student) {
-        int gaps = (name.length() - student.length())/2;
-        System.out.print("*");
-        for (int i = 0; i <= gaps; i++) {
-            System.out.print(" ");
-        }
-        System.out.print(student);
+        System.out.print(text);
         for (int i = 0; i <= gaps; i++) {
             System.out.print(" ");
         }
